4d7ff92e849e313e15f6c637db7a2388b7865cd6
tanitta <ttata.trit@gmail.com>
2024-06-09 04:46:58 +0900

    Enable to load houdini packages from .venv dir.

--- scripts/456.py
+++ scripts/456.py
@@ -17,4 +17,29 @@ def load_venv_site_packages():
     sys.path.append(site_packages_path)
     print(sys.path)
     print("[hrye] Finish site_packages loading successflly.")
+
 load_venv_site_packages()
+
+import glob
+from pathlib import Path
+def get_package_json(dir_path):
+    dir_basename = os.path.basename(dir_path)
+    return dir_path + '/' + dir_basename + '.json'
+
+def is_houdini_package(dir_path):
+    json_path = get_package_json(dir_path)
+    return os.path.isfile(json_path)
+
+def load_houdini_packages():
+    print("[hrye] Loading venv houdini packages...")
+    venvroot = hou.text.expandString("$JOB" + '/.venv')
+    site_packages_path = hou.text.expandString(venvroot + '/Lib/site-packages')
+    ds = filter(lambda d: os.path.isdir(d), glob.glob(site_packages_path + '/*'))
+    package_dirs = filter(lambda d: is_houdini_package(d), ds)
+    package_jsons = map(lambda d: get_package_json(d), package_dirs)
+    for json in package_jsons:
+        hou.ui.loadPackage(json)
+    print(list(package_jsons))
+    print("[hrye] Finish loading of houdini packages successflly.")
+
+load_houdini_packages()

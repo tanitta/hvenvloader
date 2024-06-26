0401a5d6c27d2822eebf35d5cfec5b68e14337e7
tanitta <ttata.trit@gmail.com>
2024-06-27 05:54:47 +0900

    Enable to load houdini package.

--- scripts/456.py
+++ scripts/456.py
@@ -2,8 +2,9 @@ import sys
 import os
 import hou
 
+package_name = 'hvenvloader'
 def load_venv_site_packages():
-    print("[hrye] Loading venv site_packages...")
+    print("[{package_name}] Loading venv site_packages...".format(package_name=package_name))
     venvroot = hou.text.expandString("$JOB" + '/.venv')
     site_packages_path = hou.text.expandString(venvroot + '/Lib/site-packages')
 
@@ -16,12 +17,13 @@ def load_venv_site_packages():
     hou.session.venv_site_packages_path = site_packages_path
     sys.path.append(site_packages_path)
     print(sys.path)
-    print("[hrye] Finish site_packages loading successflly.")
+    print("[{package_name}] Finish site_packages loading successflly.".format(package_name=package_name))
 
 load_venv_site_packages()
 
 import glob
 from pathlib import Path
+
 def get_package_json(dir_path):
     dir_basename = os.path.basename(dir_path)
     return dir_path + '/' + dir_basename + '.json'
@@ -31,15 +33,24 @@ def is_houdini_package(dir_path):
     return os.path.isfile(json_path)
 
 def load_houdini_packages():
-    print("[hrye] Loading venv houdini packages...")
+    print("[{package_name}] Loading venv houdini packages...".format(package_name=package_name))
     venvroot = hou.text.expandString("$JOB" + '/.venv')
     site_packages_path = hou.text.expandString(venvroot + '/Lib/site-packages')
     ds = filter(lambda d: os.path.isdir(d), glob.glob(site_packages_path + '/*'))
     package_dirs = filter(lambda d: is_houdini_package(d), ds)
     package_jsons = map(lambda d: get_package_json(d), package_dirs)
+
     for json in package_jsons:
-        hou.ui.loadPackage(json)
+        json_original = ''
+        json_modified = ''
+        with open(json) as f:
+            json_original = f.read()
+        json_modified = json_original.replace('$HOUDINI_PACKAGE_PATH', site_packages_path)
+        json_path_temp = hou.text.expandString("$TEMP" + '/hvenvloadertmp.json')
+        with open(json_path_temp, mode='w') as f:
+            f.write(json_modified)
+        hou.ui.loadPackage(json_path_temp)
     print(list(package_jsons))
-    print("[hrye] Finish loading of houdini packages successflly.")
+    print("[{package_name}] Finish loading of houdini packages successflly.".format(package_name=package_name))
 
 load_houdini_packages()
